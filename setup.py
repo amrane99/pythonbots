f06829ed38fee8efaca9db97d9503d20bc0824d8
Amin <aminranem@googlemail.com>
2022-11-21 13:55:58 +0100

    Modifed setup file.

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='PythonBots',
+    name='pythonbots',
     version='0.1',
     description='A project boundling several Bots for Python.',
-    url='https://github.com/amrane99/PythonBots',
+    url='https://github.com/amrane99/pythonbots',
     keywords='python setuptools',
     packages=find_packages(include=['bots', 'bots.*']),
 )
